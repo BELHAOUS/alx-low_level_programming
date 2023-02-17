6a57984ffe94d5628b1fcb9a11069f32377e8e35
Zakaria Belhaous <zakaria.belhaous@gmail.com>
2023-02-17 15:59:05 -0800

    positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,13 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
